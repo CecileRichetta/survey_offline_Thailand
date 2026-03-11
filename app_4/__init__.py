f929b353b1742ae1e8464ddc7801db07b6c16004
CecileRichetta <cecile.richetta@gmail.com>
2026-03-11 19:20:22 +0100

    thai: final hope treatment changes

--- app_4/__init__.py
+++ app_4/__init__.py
@@ -10,7 +10,6 @@ class C(BaseConstants):
     NAME_IN_URL = 'app_4'
     PLAYERS_PER_GROUP = None
     NUM_ROUNDS = 1
-    IMAGE_UNHRC = 'imgs/HRC_logo.png'
     # CONSTANTS
     BINARY_ANSWER = [
         (0, 'ไม่เคย'), # No
@@ -33,28 +32,28 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
+    text_check = models.IntegerField(
+        label="4.1. นักวิจัยได้ข้อสรุปเกี่ยวกับโอกาสของความสำเร็จในการสร้างความปรองดองในภาคใต้แล้วใช่หรือไม่?",
+        # Have the researchers already reached a conclusion about the chances of success of reconciliation in the Deep South?
+        choices=C.BINARY_ANSWER,
+        widget=widgets.RadioSelect,
+        blank=False
+    )
     hope_recall = models.IntegerField(
-        label="4.1.ท่านเคยรู้สึกมีความหวังบ้างหรือไม่ว่ากระบวนการสันติภาพในจังหวัดชายแดนภาคใต้จะประสบความสำเร็จได้ กรุณาใช้เวลาสักครู่เพื่อทบทวนเรื่องนี้",
+        label="4.2.ท่านเคยรู้สึกมีความหวังบ้างหรือไม่ว่ากระบวนการสันติภาพในจังหวัดชายแดนภาคใต้จะประสบความสำเร็จได้ กรุณาใช้เวลาสักครู่เพื่อทบทวนเรื่องนี้",
         # Can you recall a moment in time that made you feel hopeful that the peace process in southern Thailand is achievable? Please take some time to think about it.
         choices=C.BINARY_ANSWER,
         widget=widgets.RadioSelect,
         blank=False
     )
     hope_reason = models.LongStringField(
-        label="4.2. ถ้าเคยมีความรู้สึกเช่นนั้น ช่วยกรุณาเล่าให้เราฟังภายใน 1-2 ประโยค",
+        label="4.3. ถ้าเคยมีความรู้สึกเช่นนั้น ช่วยกรุณาเล่าให้เราฟังภายใน 1-2 ประโยค",
         # If yes, could you please share this memory with me, in one or two sentences?
         blank=True
     )
-    hope_read = models.IntegerField(
-        label="คำถามสำหรับผู้เก็บข้อมูล: ผู้ตอบแบบสอบถามตั้งใจฟังระหว่างการอ่านข้อความหรือไม่?",
-        # For the enumerator: was the person attentive during the readout?
-        choices=C.HOPE_READ,
-        widget=widgets.RadioSelect,
-        blank=False
-    )
 
 # PAGES
-class Page1(Page):
+class Page1_1(Page):
     form_model = 'player'
     @staticmethod
     def get_form_fields(player: Player):
@@ -62,9 +61,9 @@ class Page1(Page):
         participant = player.participant #
         if participant.treatment_hope == 1 and player.session.config['name'] == "session_C4P_THAI_w1":
             return [
+                'text_check',
                 'hope_recall',
-                'hope_reason',
-                'hope_read'
+                'hope_reason'
             ]
         else:
             return []
@@ -72,6 +71,25 @@ class Page1(Page):
         participant = player.participant
         return participant.treatment_hope == 1 and player.session.config['name'] == "session_C4P_THAI_w1"
 
+
+class Page1_2(Page):
+    form_model = 'player'
+    @staticmethod
+    def get_form_fields(player: Player):
+        """Only return form fields if the page is displayed"""
+        participant = player.participant #
+        if participant.treatment_hope == 0 and player.session.config['name'] == "session_C4P_THAI_w1":
+            return [
+                'text_check'
+            ]
+        else:
+            return []
+    def is_displayed(player):
+        participant = player.participant
+        return participant.treatment_hope == 0 and player.session.config['name'] == "session_C4P_THAI_w1"
+
+
 page_sequence = [
-    Page1
+    Page1_1,
+    Page1_2
 ]
